fe91ebc6d70e2bc3ee17829cc80c8175b15d7d2e
Micheal Curley <G00340243@gmit.ie>
2017-11-18 02:45:43 +0000

    Q5 complete

--- Problem-sheet-3.go
+++ Problem-sheet-3.go
@@ -10,6 +10,7 @@ import (
 	"regexp"
 	"math/rand"
 	"time"
+	"strings"
 )
 //random number generator method
 func random(min, max int) int {
@@ -25,13 +26,12 @@ func ElizaResponse(input string)(string){
 		"Why do you say that?",
 		"Why don’t you tell me more about your father?"}
 	//using regex methods to look for certain patterns
-	match, err := regexp.MatchString("(?i).*\b[Ff]ather\b.*", input)
-	//certain match returns certain response or error message
-	if (match == true && err == nil){
+	re := regexp.MustCompile("(?i).*\b[Ff]ather\b.*")
+	if (re.MatchString(input) == true) {
 		return responses[3]
-	} else if(match == false && err == nil){
+	} else if (re.MatchString(input) == false) {
 		//check for "I am", "I'm", "Iam", "i'm", "i am", "iam" in input string
-		re := regexp.MustCompile("(?i)(i[' a]*m)(.*)")
+		re := regexp.MustCompile("(?i)(I[' a]*m)([^.?!]*)[.?!]?")
 		if (re.MatchString(input) == true) {
 			return re.ReplaceAllString(input, "How do you know you are $2?")
 		} else {
@@ -40,6 +40,30 @@ func ElizaResponse(input string)(string){
 	} else {
 		return "Error message"
 	}
+}
+//Adapted from https://gist.github.com/ianmcloughlin/c4c2b8dc586d06943f54b75d9e2250fe
+//Pronouns reflection method
+func Reflect(input string) string {
+	// Split the input on word boundaries.
+	boundaries := regexp.MustCompile(`\b`)
+	tokens := boundaries.Split(input, -1)
+	// List the reflections.
+	reflections := [][]string{
+		{"your", "my"},
+		{"you", "I"},
+		{"me", "you"},
+	}
+	// Loop through each token, reflecting it if there's a match.
+	for i, token := range tokens {
+		for _, reflection := range reflections {
+			if matched, _ := regexp.MatchString(reflection[0], token); matched {
+				tokens[i] = reflection[1]
+				break
+			}
+		}
+	}
+	// Put the tokens back together.
+	return strings.Join(tokens, ``)
 }
 //main function
 func main() {
@@ -50,6 +74,7 @@ func main() {
 	scanner.Scan() 
     input := scanner.Text()
 	fmt.Println("You entered", input)
-	//calling response method
-    fmt.Println(ElizaResponse(input))
+	//calling reflect and elizaresponse  method
+	response := Reflect(input)
+	fmt.Println(ElizaResponse(response))
 }
