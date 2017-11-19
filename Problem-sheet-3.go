5e722e7e2584b8fcc75d5e710a9bb8e45d2b8fb1
Micheal Curley <G00340243@gmit.ie>
2017-11-19 21:28:22 +0000

    Q6 complete

--- Problem-sheet-3.go
+++ Problem-sheet-3.go
@@ -20,26 +20,57 @@ func random(min, max int) int {
 //response method 
 func ElizaResponse(input string)(string){
 	//saved responses array
-	responses := [4] string {
+	responses := [3] string {
 		"I’m not sure what you’re trying to say. Could you explain it to me?",
 		"How does that make you feel?",
 		"Why do you say that?",
-		"Why don’t you tell me more about your father?"}
-	//using regex methods to look for certain patterns
-	re := regexp.MustCompile("(?i).*\b[Ff]ather\b.*")
-	if (re.MatchString(input) == true) {
-		return responses[3]
-	} else if (re.MatchString(input) == false) {
-		//check for "I am", "I'm", "Iam", "i'm", "i am", "iam" in input string
-		re := regexp.MustCompile("(?i)(I[' a]*m)([^.?!]*)[.?!]?")
-		if (re.MatchString(input) == true) {
-			return re.ReplaceAllString(input, "How do you know you are $2?")
-		} else {
-			return responses[random(0, len(responses))]
-		} 
-	} else {
-		return "Error message"
 	}
+	//regexp strings to look for certain patterns
+	reObjs := [6]string{
+		//check for father regardless of lower/upper case
+		"(?i).*\b[Ff]ather\b.*",
+		//check for "I am", "I'm", "Iam", "i'm", "i am", "iam" in input string and capturing data
+		"(?i)(I[' a]*m)([^.?!]*)[.?!]?",
+		//check for "i think" and capturing data
+		"(?i)(I think)([^.?!]*)[.?!]?",
+		//check for you and ?
+		"(?i)(you+)(.*)(\\?+)",
+		//check for "i like" and capturing data
+		"(?i)(I like)(\\s+)[.?!]?",
+		//check for numbers
+		"\\d",
+	}
+	//for loop iterating over regexp strings and compiling them
+	for i:=0; i < len(reObjs); i++ {
+		//test print to console
+		//fmt.Println(reObjs[i])
+		re := regexp.MustCompile(reObjs[i])
+		match := re.MatchString(input)
+		if (match == true) {
+			switch i {
+				case 0:
+					//father response
+					return "Why don’t you tell me more about your father?"
+				case 1:
+					//replacing start of response and adding captured data
+					return re.ReplaceAllString(input, "How do you know you are $2?")
+				case 2:
+					return re.ReplaceAllString(input, "What makes you think that $2?")
+				case 3:
+					//talking about you not me response
+					return "I thought we were talking about you, not me!"
+				case 4:
+					//I like response
+					return re.ReplaceAllString(input, "Why is it that you like $2?")
+				case 5:
+					//numbers response
+					return "I'm not a fan of numbers, can you simplify it in any way?"
+				default:
+					return "Error message"
+			}
+		} 
+	}
+	return responses[random(0, len(responses))]	
 }
 //Adapted from https://gist.github.com/ianmcloughlin/c4c2b8dc586d06943f54b75d9e2250fe
 //Pronouns reflection method
